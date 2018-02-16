097429652b90155f6cf6ede520df3629531034a5
John Sylvain <john.n.sylvain@gmail.com>
2018-02-15 18:40:42 -0600

    add async await

--- src/actions.js
+++ src/actions.js
@@ -16,43 +16,42 @@ export default store => ({
       : 0
   }),
 
-  getVideos: (state, subreddit) => {
+  async getVideos (state, subreddit) {
     store.setState({
       loading: true
     })
 
-    fetch(`https://www.reddit.com/r/${subreddit}/hot.json?limit=300`)
-      .then(res => res.json())
-      .then(json => {
-        const videos = json.data.children
-          .filter(v => 
-            !v.data.stickied && 
-            !v.data.over_18 && 
-            v.data.post_hint === 'rich:video'
-          )
-          .map((item, i) => ({
-            meta: {
-              author: item.data.author,
-              score: item.data.score,
-              title: item.data.title,
-              permalink: item.data.permalink,
-              created: item.data.created_utc * 1000,
-              _id: item.data.id
-            },
-            media: {
-              iframe: unEntity(item.data.media_embed.content || ''),
-              thumbnail: item.data.thumbnail
-            }
-          }))
+    const json = await (await fetch(`https://www.reddit.com/r/${subreddit}/hot.json?limit=300`)).json()
 
-        store.setState({
-          videos,
-          pointer: 0,
-          loading: false
-        })
-      })
-      .catch(() => {
-        route('/404', true)
-      })
+    if (!json) {
+      route('/404', false)
+    }
+
+    const videos = json.data.children
+      .filter(v =>
+        !v.data.stickied &&
+        !v.data.over_18 &&
+        v.data.post_hint === 'rich:video'
+      )
+      .map((item, i) => ({
+        meta: {
+          author: item.data.author,
+          score: item.data.score,
+          title: item.data.title,
+          permalink: item.data.permalink,
+          created: item.data.created_utc * 1000,
+          _id: item.data.id
+        },
+        media: {
+          iframe: unEntity(item.data.media_embed.content || ''),
+          thumbnail: item.data.thumbnail
+        }
+      }))
+
+    store.setState({
+      videos,
+      pointer: 0,
+      loading: false
+    })
   }
 })
