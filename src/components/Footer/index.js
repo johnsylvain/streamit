ce03f384543b1828a4b16f76e409f3bcf907b31b
John Sylvain <john.n.sylvain@gmail.com>
2018-02-18 11:29:41 -0600

    consolidate page components

--- src/components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import { h } from 'preact'
-import style from './style'
-
-const Footer = () =>
-  <footer className={style.footer}>
-    John Sylvain &copy; {new Date().getFullYear()}
-  </footer>
-
-export default Footer
--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,9 +1,16 @@
 import { h, Component } from 'preact'
-
-import Header from '../Header'
-import Footer from '../Footer'
-
 import style from './style'
+
+const Header = ({ subreddit }) =>
+  <header className={style.header}>
+    <h1><a href="/">Streamit</a></h1>
+    {subreddit && <p>/r/{subreddit}</p>}
+  </header>
+
+const Footer = () =>
+  <footer className={style.footer}>
+    John Sylvain &copy; {new Date().getFullYear()}
+  </footer>
 
 export default class Page extends Component {
   static Header = Header
