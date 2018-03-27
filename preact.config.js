62842e4758288bf7fdcccf80c0fe0f4b12a4629e
John Sylvain <john.n.sylvain@gmail.com>
2018-03-27 16:01:49 -0500

    fix config indentation

--- preact.config.js
+++ preact.config.js
@@ -1,26 +1,26 @@
 export default (config, env, helpers) => {
-	let babel = helpers.getLoadersByName(config, 'babel-loader')[0].rule.options;
+  let babel = helpers.getLoadersByName(config, 'babel-loader')[0].rule.options;
   let uglify = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
 
-	babel.presets[0][1].exclude.push(
-		'transform-async-to-generator',
-		'transform-regenerator'
-	);
+  babel.presets[0][1].exclude.push(
+    'transform-async-to-generator',
+    'transform-regenerator'
+  );
 
-	babel.plugins.push([
-		'fast-async',
-		{
-			env: {
-				log: true
-			},
-			compiler: {
-				promises: true,
-				noRuntime: true
-			}
-		}
-	]);
+  babel.plugins.push([
+    'fast-async',
+    {
+      env: {
+        log: true
+      },
+      compiler: {
+        promises: true,
+        noRuntime: true
+      }
+    }
+  ]);
 
-	if (uglify) {
-		config.plugins.splice(uglify.index, 1);
-	}
+  if (uglify) {
+    config.plugins.splice(uglify.index, 1);
+  }
 };
